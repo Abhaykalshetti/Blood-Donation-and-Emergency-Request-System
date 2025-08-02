a0a75e89b0997d750868b57fb56e1cca868c2e47
Abhaykalshetti <153755268+Abhaykalshetti@users.noreply.github.com>
2025-08-02 16:32:55 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,9 +1,6 @@
 import axios from 'axios';
-<<<<<<< HEAD
 import url from './url';
-=======
 const url="https://blood-donation-and-emergency-request.onrender.com";
->>>>>>> e8944991d75da7da464e3df3fe608b92c8fb5577
 const api = axios.create({
   baseURL: url,
   withCredentials: true, // for JWT cookies if used
